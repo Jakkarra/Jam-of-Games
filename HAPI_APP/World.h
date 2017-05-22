38be5346a97128d2437e1c70fd9db90cec5a82d8
NICULAE <Q5089832@live.tees.ac.uk>
2017-05-22 14:41:24 +0100

    Path finding and room sprites

--- HAPI_APP/World.h
+++ HAPI_APP/World.h
@@ -7,6 +7,7 @@
 #include "EntityHealth.h"
 #include "EntityPickup.h"
 #include "Room.h"
+#include <random>
 
 class CEntity;
 
@@ -28,6 +29,9 @@ public:
 	void charCreation();
 	void endGame();
 	void pause();
+	void Create_Rooms(int Number_of_Rooms, int Texture_Size);
+	void Connect_Rooms();
+	int Generate_random_vector(int minimum_value, int maximum_values);
 	std::vector<CEntityBullet*> getBullets() { return bulletVector; }
 	Point getPlayerPos() { return player_->getPos(); }
 	int getPlayerHealth() { return player_->getHealth(); }
@@ -41,6 +45,7 @@ private:
 	std::vector<CEntity*> entityVector;
 	std::vector<CEntityBullet*> bulletVector;
 	std::vector<CEntity*> healthVector;
+	std::vector<Room> Rooms;
 
 	CEntityPlayer* player_ = new CEntityPlayer;
 
@@ -54,7 +59,7 @@ private:
 	int currentHealth = 6;
 
 	Point Position_To_Spawn{ 250,250 };
-	Point Position_To_Spawn_second{ 450,450 };
+	Point Position_To_Spawn_second{ 2000, 450 };
 
 	//Character Creation Variables
 	unsigned int totalPoints = 8;
@@ -70,5 +75,8 @@ private:
 
 	CEntityMenu *bg = new CEntityMenu("Data//Background.jpg");
 	CEntityMenu *sp = new CEntityMenu("Data//XboxRTLogo.png");
+
+	std::default_random_engine rand_engine;
+	int number_of_rooms;
 };
 
